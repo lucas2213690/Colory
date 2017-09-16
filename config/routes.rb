7c33d95403becf54e9ae3e1957357ca1d55b7d23
Lucas Alves <lucas2213690@gmail.com>
2017-09-16 14:47:08 -0300

    Ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'startup/index'
+  #get 'startup/index'
 
   resources :turmas
+  root "startup#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
